517e13433f5edadcfbba4cd27960656ccd223ffc
seraperkan <serapperkan@gmail.com>
2023-03-30 15:22:00 +0300

    Added layout structures

---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header/>
+        <Banner/>
+        <nuxt/>
+        <Footer/>
+      
+    </div>
+</template>
+<script>
+import Header from '@/components/common/Header.vue'
+import Banner from '@/components/common/Banner.vue'
+import Footer from '@/components/common/Footer.vue';
+export default{
+
+  components:{
+    Header,
+    Banner,
+    Footer
+  }
+}
+</script>
---
+++ nuxt-app/layouts/userLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header/>
+        <nuxt/>
+        <Footer/>
+      
+    </div>
+</template>
+<script>
+import Header from '@/components/common/Header.vue'
+import Footer from '@/components/common/Footer.vue';
+export default{
+
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
