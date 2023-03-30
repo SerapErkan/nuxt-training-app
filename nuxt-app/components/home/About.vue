53b40e9263fd2b46092fbb5cc8bb874bc2d77a45
seraperkan <serapperkan@gmail.com>
2023-03-30 15:36:27 +0300

    About page added

---
+++ nuxt-app/components/home/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex text-center bg-dark p-5">
+    <div
+      class="cover-container d-flex w-100 p-5 mx-auto flex-column text-white"
+    >
+      <main class="px-3 page">
+        <h1>About your page.</h1>
+        <p class="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+          molestias dicta delectus ut repellat quam deserunt, amet mollitia!
+          Ipsa praesentium alias dolor soluta quis beatae nam libero, quod
+          voluptatem sapiente nesciunt magni veritatis optio qui voluptate illo
+          tenetur nihil reprehenderit odit provident placeat nisi. Unde, quam!
+          Asperiores sapiente eaque laborum at quidem temporibus beatae dicta
+          sed nemo dolor, nam, voluptatum alias sunt fuga ut, voluptate
+          distinctio id iste magni? Rerum ex, neque asperiores, nisi vel
+          perspiciatis explicabo quos magni hic adipisci ratione accusantium
+          porro soluta! Omnis dignissimos ex delectus maiores fugit mollitia
+          consectetur, id et est corrupti odit enim magnam?
+        </p>
+        <p class="lead">
+          <nuxt-link to="/">
+            <a class="btn btn-lg btn-light fw-bold border-white bg-white"
+              >Home</a
+            >
+          </nuxt-link>
+        </p>
+      </main>
+    </div>
+  </div>
+</template>
+<style scoped>
+.page {
+  min-height: 80vh;
+}
+</style>
---
+++ nuxt-app/pages/about/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <About />
+</template>
+<script>
+import About from "../../components/home/about.vue";
+export default {
+  components: {
+    About,
+  },
+  layout: "pageLayout",
+};
+</script>
