5ade68467f00c12b3d7f0e8d65bb1205ef7daca3
seraperkan <serapperkan@gmail.com>
2023-04-03 12:36:39 +0300

    asyncData-fetch

--- nuxt-app/pages/posts/_postId/index.vue
+++ nuxt-app/pages/posts/_postId/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostDetail />
+  <PostDetail :post="fetchedPost" />
 </template>
 
 <script>
@@ -9,5 +9,19 @@ export default {
     PostDetail,
   },
   layout: "userLayout",
+
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        fetchedPost: {
+          id: "1",
+          title: "kötü kara kedi köfte",
+          subTitle: "patilio",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quam ex iusto vel esse dolore facilis reprehenderit architecto consequatur, neque possimus.",
+          author: "SerapErkan",
+        },
+      });
+    }, 2000);
+  },
 };
 </script>
