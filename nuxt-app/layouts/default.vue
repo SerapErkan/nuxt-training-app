651522c31b05bb9090f49f39b595c25cd0e02e89
seraperkan <serapperkan@gmail.com>
2023-04-03 12:35:03 +0300

    code edited

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -1,22 +1,33 @@
 <template>
-    <div>
-        <Header/>
-        <Banner/>
-        <nuxt/>
-        <Footer/>
-      
+  <div>
+    <Header />
+
+    <div class="center">
+      <Banner />
     </div>
+    <nuxt />
+
+    <Footer />
+  </div>
 </template>
 <script>
-import Header from '@/components/common/Header.vue'
-import Banner from '@/components/common/Banner.vue'
-import Footer from '@/components/common/Footer.vue';
-export default{
-
-  components:{
+import Header from "@/components/common/Header.vue";
+import Banner from "@/components/common/Banner.vue";
+import Footer from "@/components/common/Footer.vue";
+export default {
+  components: {
     Header,
     Banner,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
+<style>
+.center {
+  display: flex; /* Flex konteyner olarak ayarla */
+  flex-direction: column; /* Dikey hizalanacak öğeler */
+  justify-content: center; /* Dikeyde de ortala */
+  align-items: center; /* Yatayda ortala */
+  height: 50vh; /* Ekran yüksekliği kadar genişlet */
+}
+</style>
